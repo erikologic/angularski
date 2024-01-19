dc9844b29058bd8d4455a3f37623e6f6621f734b
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-01-19 22:49:45 +0000

    added Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, CommonModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  firestore: Firestore = inject(Firestore);
   title = 'angularski';
+
+  items$: Observable<{[name: string]: string}[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items$ = collectionData(aCollection);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        "projectId": "angularski",
+        "appId": "1:126687368480:web:d0a8f9088155678f5eb345",
+        "storageBucket": "angularski.appspot.com",
+        "apiKey": import.meta.env.NG_APP_FIREBASE_API_KEY, 
+        "authDomain": "angularski.firebaseapp.com",
+        "messagingSenderId": "126687368480"
+    }
+};
